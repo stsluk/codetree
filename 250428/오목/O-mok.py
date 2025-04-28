711a3cdf5c2007b012409f39b7a7988e388fda4a
stsluk <124018109+stsluk@users.noreply.github.com>
2025-04-28 22:38:54 +0900

    Title: 오목, Time: 78ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250428/오목/O-mok.py
@@ -0,0 +1,38 @@
+board = [list(map(int, input().split())) for _ in range(19)]
+
+# Please write your code here.
+def who_is_win(i, j):
+    win = board[i][j]
+    if win == 0: return False, False, False
+    for ki in [0, 1]:
+        for kj in [-1, 0, 1]:
+            if ki == kj == 0: continue
+            if judgement_win(i, j, ki, kj):
+                return win, (i + 2*ki +1), (j + 2*kj +1)
+    return False, False, False
+
+
+def judgement_win(i, j, ki, kj):
+    for k in range(1, 5):
+        x = i + ki*k
+        y = j + kj*k
+        if not ((0 <= x < 19) or (0 <= y < 19)):
+            return False
+        if board[i][j] != board[x][y]:
+            return False
+    return True
+
+
+def print_winner():
+    for i in range(19):
+        for j in range(19):
+            win, x, y = who_is_win(i, j)
+            if win:
+                print(win)
+                print(x, y)
+                return 
+    print(0)
+    return 
+
+
+print_winner()
