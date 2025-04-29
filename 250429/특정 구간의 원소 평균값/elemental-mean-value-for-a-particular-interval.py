3f2a679cce15138e83cd73f67ff06b57bce29bba
stsluk <124018109+stsluk@users.noreply.github.com>
2025-04-29 22:58:11 +0900

    Title: 특정 구간의 원소 평균값, Time: 52ms, Memory: 17MB, Status: Passed - Codetree

--- 250429/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ 250429/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -5,8 +5,10 @@ arr = list(map(int, input().split()))
 ans = 0
 for i in range(n):
     sum_arr = 0
+    scope = set()
     for j in range(i, n):
         sum_arr += arr[j]
-        if (sum_arr/(j-i+1)) in arr[i:j+1]:
+        scope.add(arr[j])
+        if (sum_arr/(j-i+1)) in scope:
             ans += 1
 print(ans)
