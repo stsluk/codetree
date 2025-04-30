c324520e2a6f818df10b45891b982b6fca9c0cbb
stsluk <124018109+stsluk@users.noreply.github.com>
2025-04-30 23:06:03 +0900

    Title: 바구니 안의 사탕 2, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250430/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 250430/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -7,7 +7,11 @@ for _ in range(N):
     dic[p] += c
 
 max_candy = 0
-for i in range(101-2*K):
+for i in range(101):
+    if (i+2*K) > 100:
+        sum_candy = sum(dic[i:101])
+        max_candy = max(max_candy, sum_candy)
+        break
     sum_candy = sum(dic[i:i+2*K+1])
     max_candy = max(max_candy, sum_candy)
 print(max_candy)
