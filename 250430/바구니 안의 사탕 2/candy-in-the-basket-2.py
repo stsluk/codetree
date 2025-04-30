79fda248e6d27622ca3093a869deddd58d1b7798
stsluk <124018109+stsluk@users.noreply.github.com>
2025-04-30 23:00:08 +0900

    Title: 바구니 안의 사탕 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250430/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 250430/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -4,10 +4,10 @@ dic = [0]*101
 # candy position
 for _ in range(N):
     c, p = map(int, input().split())
-    dic[p] = c
+    dic[p] += c
 
 max_candy = 0
-for i in range(100-2*K):
+for i in range(101-2*K):
     sum_candy = sum(dic[i:i+2*K+1])
     max_candy = max(max_candy, sum_candy)
 print(max_candy)
