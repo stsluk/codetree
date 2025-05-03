0d2cb1a3f2d57d16a6613883941b65b4d283e731
stsluk <124018109+stsluk@users.noreply.github.com>
2025-05-03 22:28:14 +0900

    Title: 두 선분, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250503/두 선분/two-lines.py
+++ 250503/두 선분/two-lines.py
@@ -2,8 +2,8 @@ x1, x2, x3, x4 = map(int, input().split())
 
 # Please write your code here.
 def is_intersecting(x1, x2, x3, x4):
-    if x1 < x3 <= x2: return True
-    if x3 < x1 <= x4: return True
+    if x1 <= x3 <= x2: return True
+    if x3 <= x1 <= x4: return True
     return False
 
 if is_intersecting(x1, x2, x3, x4): print("intersecting")
