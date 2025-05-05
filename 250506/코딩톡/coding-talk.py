662a0a00f6d5157a67f38b76497bf153f92f5f89
stsluk <124018109+stsluk@users.noreply.github.com>
2025-05-06 00:03:12 +0900

    Title: 코딩톡, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250506/코딩톡/coding-talk.py
@@ -0,0 +1,12 @@
+n, m, p = map(int, input().split())
+
+# Please write your code here.
+dic = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+dic = dic[:n]
+for _ in range(m):
+    c, u = input().split()
+    if _ < p-1: continue
+    if c in dic:
+        i = dic.index(c)
+        del dic[i]
+print(*dic)
