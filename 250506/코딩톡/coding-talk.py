725644a86ea1a65e883dedbcb2b1a8072bb3f3a8
stsluk <124018109+stsluk@users.noreply.github.com>
2025-05-06 00:27:34 +0900

    Title: 코딩톡, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250506/코딩톡/coding-talk.py
+++ 250506/코딩톡/coding-talk.py
@@ -1,15 +1,25 @@
 n, m, p = map(int, input().split())
+messages = [tuple(input().split()) for _ in range(m)]
 
 # Please write your code here.
-dic = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-dic = dic[:n]
-for _ in range(m):
-    c, u = input().split()
-    if _ < p-1: continue
-    if (_ == p-1) and (u == '0'):
-        dic = []
-        break
-    if c in dic:
-        i = dic.index(c)
-        del dic[i]
-print(*dic)
+if messages[p-1][1] == '0':
+    print()
+    exit()
+
+
+no_see = [True for _ in range(n)]
+see = set()
+for i in range(p):
+    c, u = messages[i]
+    if not (i > 0 and messages[i-1][1] == u): see = set()
+    see.add(c)
+for c in list(see):
+    no_see[ord(c)-65] = False
+
+for i in range(p, m):
+    c, u = messages[i]
+    no_see[ord(c)-65] = False
+
+for i in range(n):
+    if no_see[i]:
+        print(chr(i+65), end=' ')
