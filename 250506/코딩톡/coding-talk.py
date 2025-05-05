8c874659b71ee79b92a3735cbfca5cc06e0fd3e3
stsluk <124018109+stsluk@users.noreply.github.com>
2025-05-06 00:37:10 +0900

    Title: 코딩톡, Time: 86ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250506/코딩톡/coding-talk.py
+++ 250506/코딩톡/coding-talk.py
@@ -1,25 +1,20 @@
 n, m, p = map(int, input().split())
 messages = [tuple(input().split()) for _ in range(m)]
 
-# Please write your code here.
-if messages[p-1][1] == '0':
-    print()
-    exit()
 
+# 모두 읽은 채팅이라면 읽지 않은 사람은 없습니다.
+if int(messages[p - 1][1]) == 0:
+    sys.exit()
 
-no_see = [True for _ in range(n)]
-see = set()
-for i in range(p):
-    c, u = messages[i]
-    if not (i > 0 and messages[i-1][1] == u): see = set()
-    see.add(c)
-for c in list(see):
-    no_see[ord(c)-65] = False
-
-for i in range(p, m):
-    c, u = messages[i]
-    no_see[ord(c)-65] = False
-
+read = [False for _ in range(n)]
+# 각 사람에 대해 채팅을 읽었을지 안 읽었을지 판단합니다.
+# 만약 p번 메시지를 읽은 사람 수와 같은 채팅을 기준으로
+# 한번이라도 채팅을 쳤다면 확실하게 채팅을 읽었습니다.
+for c, u in messages:
+    u = int(u)
+    if u >= int(messages[p - 1][1]):
+        read[ord(c) - ord('A')] = True
+    
 for i in range(n):
-    if no_see[i]:
-        print(chr(i+65), end=' ')
+    if not read[i]:
+        print(chr(ord('A') + i), end=' ')
