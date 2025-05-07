ad1d78c3cd9f2404f2bbf5ba287e9bd423d5f019
stsluk <124018109+stsluk@users.noreply.github.com>
2025-05-07 14:47:37 +0900

    Title: 정수 명령 처리 5, Time: 129ms, Memory: 21MB, Status: Passed - Codetree

---
+++ 250507/정수 명령 처리 5/process-numeric-commands-5.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+num = []
+
+for _ in range(N):
+    line = input().split()
+    if line[0] == "push_back":
+        num.append(int(line[1]))
+    elif line[0] == "pop_back":
+        num.pop()
+    elif line[0] == "size":
+        print(len(num))
+    elif line[0] == "get":
+        print(num[int(line[1])-1])
+    else:
+        print("잘못된 명령어입니다.")
