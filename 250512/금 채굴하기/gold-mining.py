66479f54dbea22b024ef5cd7a75b2691c2c2ec5f
stsluk <124018109+stsluk@users.noreply.github.com>
2025-05-12 22:20:32 +0900

    Title: 금 채굴하기, Time: 119ms, Memory: 19MB, Status: Wrong Answer - Codetree

--- 250512/금 채굴하기/gold-mining.py
+++ 250512/금 채굴하기/gold-mining.py
@@ -3,9 +3,9 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
 def best_mining(i, j):
-    ans = 0
+    ans = grid[i][j]
     gold = grid[i][j]
-    for k in range(n):
+    for k in range(1, n):
         for l in range(k):
             if (0 <= i-k+l < n) and (0 <= j-l < n):
                 gold += grid[i-k+l][j-l]
@@ -27,4 +27,5 @@ ans = 0
 for i in range(n):
     for j in range(n):
         ans = max(ans, best_mining(i, j))
+        # print(i, j, ':', best_mining(i, j))
 print(ans)
