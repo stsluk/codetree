dd553378d581b8ad6bda0ea62973c774b773efe6
stsluk <124018109+stsluk@users.noreply.github.com>
2025-05-15 16:26:40 +0900

    Title: 아름다운 수, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250515/아름다운 수/beautiful-number.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+# Please write your code here.
+def how_number(n):
+    if n == 0: return 1
+
+    ans = 0
+    for i in range(1, n+1):
+        ans += how_number(n-i)
+    return ans
+
+print(how_number(n))
