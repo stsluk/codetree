8a405f447d57bb4acbf281446b392c85edb64e24
stsluk <124018109+stsluk@users.noreply.github.com>
2025-08-30 00:23:04 +0900

    Title: 사다리 타기, Time: 1095ms, Memory: 24MB, Status: Time Limit Exceed - Codetree

--- 250830/사다리 타기/ladder-game.py
+++ 250830/사다리 타기/ladder-game.py
@@ -4,35 +4,41 @@ edges = [tuple(map(int, input().split())) for _ in range(m)]
 # 사다리 타기 결과 구하
 def ladder_game(edges):
     # 사다리 타기 그리기
-    ladder = [[False for _ in range(n+1)] for _ in range(m)]
+    ladder = [[False for _ in range(n+1)] for _ in range(15)]
     for a, b in edges:
         ladder[b-1][a] = True
     
-    ans = []
+    w = []
     for i in range(n):
-        for j in range(m):
+        for j in range(15):
             if ladder[j][i]: i -= 1
             elif ladder[j][i+1]: i += 1
-        ans.append(i+1)
-    return ans
+        w.append(i+1)
+    return w
 
-def minimum_line(want, x, y, ans):
-    if want == [1, 2, 3, 4]: return ans
+
+ans = [99999999]
+def minimum_line(want, x, y, s):
+    if want == [i+1 for i in range(n)]:
+        ans[0] = min(ans[0], s)
+        return s
+    
+    if s >= ans[0]: return 99999999
 
     next_x = x + 1
     next_y = y
     if next_x >= n:
         next_x = 1; next_y += 1
-        if next_y >= m: return 99999999
-    a = minimum_line(want, next_x, next_y, ans)
+        if next_y >= 15: return 99999999
+    a = minimum_line(want, next_x, next_y, s)
 
     next_x = x + 2
     next_y = y
     if next_x >= n:
         next_x = 1; next_y += 1
-        if next_y >= m: return 99999999
+        if next_y >= 15: return 99999999
     check_want(x, want)
-    a = min(a, minimum_line(want, next_x, next_y, ans+1))
+    a = min(a, minimum_line(want, next_x, next_y, s+1))
     check_want(x, want)
 
     return a
